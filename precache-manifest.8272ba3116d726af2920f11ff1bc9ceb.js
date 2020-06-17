fccfb780dddc8539d97a1a2d51c34fec85e36b01
tpanpalia <tanushreepanpalia9@gmail.com>
2020-06-17 14:19:13 +0530

    Updates

---
+++ precache-manifest.8272ba3116d726af2920f11ff1bc9ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e376fc55d6a38d5c96628f89daf4af71",
+    "url": "/user-details/index.html"
+  },
+  {
+    "revision": "44e843920f3683e64b17",
+    "url": "/user-details/static/css/main.02247bee.chunk.css"
+  },
+  {
+    "revision": "72bf38b26a236e818acd",
+    "url": "/user-details/static/js/2.4be33c80.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/user-details/static/js/2.4be33c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e843920f3683e64b17",
+    "url": "/user-details/static/js/main.6c94f358.chunk.js"
+  },
+  {
+    "revision": "4d790f902f88ece90b1e",
+    "url": "/user-details/static/js/runtime-main.4813801b.js"
+  }
+]);
